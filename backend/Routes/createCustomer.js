0344a6967ec4c34d1dc3833f2590ad63aae3cc03
nilaysrivastava <nilay2103@gmail.com>
2025-07-13 15:23:01 +0530

    final

--- backend/Routes/createCustomer.js
+++ backend/Routes/createCustomer.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Customer = require("../models/customerSchema");
+const Customer = require("../Models/customerSchema");
 
 router.post("/createCustomer", async (req, res) => {
   try {
